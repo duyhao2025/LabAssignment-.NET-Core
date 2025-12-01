c8e9fda1eb88ea8c3af637adf286ee1aa226a017
duyhao2025 <thangnm222@uef.edu.vn>
2025-12-01 17:36:25 +0700

    Lab2_HelloWorld

---
+++ Lab2/Lab2/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "This is my default action ...";
+        }
+        public string Welcome() {
+            return "This is the Welcome action method ...";
+        }
+
+    }
+}
--- Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c821d1a6483140e8515874a945d858720e733d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f02f9ff1114f438e08a4403f476ed6e1726a4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
